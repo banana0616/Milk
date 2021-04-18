5d84e82738b4534ec002f5aa4084686bf0e364bb
바나나 <65809900+banana0616@users.noreply.github.com>
2021-04-18 13:25:43 +0900

    Update bot.ts

--- src/config/bot.ts
+++ src/config/bot.ts
@@ -1,11 +1,11 @@
 export default {
-    token: 'NzMyNTY4NTUzODUyNzY0Mjcx.Xw2foQ.Zdw10q20vwj0xnaVLCfKwQiRqKk',
+    token: '',
     prefix: '우유야 ',
     prefix2: 'm!',
     owners: ["432160630682681347", "628595345798201355", "441202161481809922", "774976370290851870", "822744996632461323"],
     shards: {
-        token: 'NzMyNTY4NTUzODUyNzY0Mjcx.Xw2foQ.Zdw10q20vwj0xnaVLCfKwQiRqKk',
+        token: '',
         execArgv: ['-r', 'ts-node/register']
     },
-    koreantoken: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjczMjU2ODU1Mzg1Mjc2NDI3MSIsImlhdCI6MTYxNTIwMDk1NywiZXhwIjoxNjQ2NzU4NTU3fQ.KzUMlpjo4gkHhucwRtzsHNds_-dZhTi0ZH3i5bkX7iz1aEDUQjAygNIb6SyNqN1BplQp0Bf98NaeXz2j3bwL5Bp4GjjVrBvXwuldQYN2lP_rRtzaQ_31YwrCSayQSdHcG3sdIRQmsBTwcj23GLSCGVhJ0ijnUh2zu8Xim5IC7KI'
+    koreantoken: ''
 }
